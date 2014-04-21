0d6c258d82afd1aa80d6bca33fc5958470c50651
Suparn <gupta.suparn@gmail.com>
2014-04-21 04:47:42 -0500

    node tests
    Added node tests

---
+++ src/test/java/network/NodeTest.java
@@ -0,0 +1,530 @@
+package network;
+
+import junit.framework.Assert;
+import org.junit.Test;
+import org.junit.Before; 
+import org.junit.After;
+import server.network.Node;
+
+/** 
+* Node Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>Apr 20, 2014</pre> 
+* @version 1.0 
+*/ 
+public class NodeTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: getNodeId() 
+* 
+*/ 
+@Test
+public void testGetNodeId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setNodeId(int nodeId) 
+* 
+*/ 
+@Test
+public void testSetNodeId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getServerToServerChannel() 
+* 
+*/ 
+@Test
+public void testGetServerToServerChannel() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setServerToServerChannel(ServerToServerChannel serverToServerChannel) 
+* 
+*/ 
+@Test
+public void testSetServerToServerChannel() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getServerPortNum() 
+* 
+*/ 
+@Test
+public void testGetServerPortNum() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setServerPortNum(int serverPortNum) 
+* 
+*/ 
+@Test
+public void testSetServerPortNum() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getHostName() 
+* 
+*/ 
+@Test
+public void testGetHostName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setHostName(String hostName) 
+* 
+*/ 
+@Test
+public void testSetHostName() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: sendMessage(String hostName, int portNum, String message) 
+* 
+*/ 
+@Test
+public void testSendMessage() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: broadcastMessage(HashMap<String, Integer> hostPortMap, String message) 
+* 
+*/ 
+@Test
+public void testBroadcastMessage() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: toString() 
+* 
+*/ 
+@Test
+public void testToString() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getServerToClientChannel() 
+* 
+*/ 
+@Test
+public void testGetServerToClientChannel() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setServerToClientChannel(ServerToClientChannel serverToClientChannel) 
+* 
+*/ 
+@Test
+public void testSetServerToClientChannel() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getClientPortNum() 
+* 
+*/ 
+@Test
+public void testGetClientPortNum() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setClientPortNum(int clientPortNum) 
+* 
+*/ 
+@Test
+public void testSetClientPortNum() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: lockObject(String objectId, int clientId) 
+* 
+*/ 
+@Test
+public void testLockObject() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: freeObject(String objectId, int clientId) 
+* 
+*/ 
+@Test
+public void testFreeObject() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: isObjectLocked(String objectId, int clientId) 
+* 
+*/ 
+@Test
+public void testIsObjectLocked() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: isInProcess() 
+* 
+*/ 
+@Test
+public void testIsInProcess() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setInProcess(boolean inProcess) 
+* 
+*/ 
+@Test
+public void testSetInProcess() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getDataAccess() 
+* 
+*/ 
+@Test
+public void testGetDataAccess() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setDataAccess(DataAccess dataAccess) 
+* 
+*/ 
+@Test
+public void testSetDataAccess() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getLockedObjects() 
+* 
+*/ 
+@Test
+public void testGetLockedObjects() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setLockedObjects(HashMap<String, Integer> lockedObjects) 
+* 
+*/ 
+@Test
+public void testSetLockedObjects() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getTemporaryData() 
+* 
+*/ 
+@Test
+public void testGetTemporaryData() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setTemporaryData(HashSet<Account> temporaryData) 
+* 
+*/ 
+@Test
+public void testSetTemporaryData() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getTemporaryAccount(String objectId) 
+* 
+*/ 
+@Test
+public void testGetTemporaryAccount() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPreparedAccount(String objectId) 
+* 
+*/ 
+@Test
+public void testGetPreparedAccount() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPreparedData() 
+* 
+*/ 
+@Test
+public void testGetPreparedData() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setPreparedData(HashSet<Account> preparedData) 
+* 
+*/ 
+@Test
+public void testSetPreparedData() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: addToPreparedData(Account acc) 
+* 
+*/ 
+@Test
+public void testAddToPreparedData() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: init() 
+* 
+*/ 
+@Test
+public void testInit() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getPreparedAccess() 
+* 
+*/ 
+@Test
+public void testGetPreparedAccess() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setPreparedAccess(DataAccess preparedAccess) 
+* 
+*/ 
+@Test
+public void testSetPreparedAccess() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: isPeerReachable(int peerId) 
+* 
+*/ 
+@Test
+public void testIsPeerReachable() throws Exception {
+    Node node = new Node(0, "", 0, 0);
+    Assert.assertTrue(node.isPeerReachable(0));
+    Assert.assertTrue(node.isPeerReachable(1));
+    Assert.assertTrue(node.isPeerReachable(2));
+} 
+
+/** 
+* 
+* Method: computeServerId(String objectId) 
+* 
+*/ 
+@Test
+public void testComputeServerId() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getMutationRequestBuffer() 
+* 
+*/ 
+@Test
+public void testGetMutationRequestBuffer() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setMutationRequestBuffer(HashMap<String, List<MutationReq>> mutationRequestBuffer) 
+* 
+*/ 
+@Test
+public void testSetMutationRequestBuffer() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getMutationWriteRequests() 
+* 
+*/ 
+@Test
+public void testGetMutationWriteRequests() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setMutationWriteRequests(HashMap<String, List<String>> mutationWriteRequests) 
+* 
+*/ 
+@Test
+public void testSetMutationWriteRequests() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: addMutationWriteReq(String requestId) 
+* 
+*/ 
+@Test
+public void testAddMutationWriteReq() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: addMutationReq(MutationReq mutationReq) 
+* 
+*/ 
+@Test
+public void testAddMutationReq() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: getLock() 
+* 
+*/ 
+@Test
+public void testGetLock() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: setLock(ReentrantLock lock) 
+* 
+*/ 
+@Test
+public void testSetLock() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: performMutation(MutationReq mutationReq) 
+* 
+*/ 
+@Test
+public void testPerformMutation() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: hashCode() 
+* 
+*/ 
+@Test
+public void testHashCode() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: equals(java.lang.Object o) 
+* 
+*/ 
+@Test
+public void testEquals() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: uncaughtException(java.lang.Thread thread, java.lang.Throwable throwable) 
+* 
+*/ 
+@Test
+public void testUncaughtException() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: values() 
+* 
+*/ 
+@Test
+public void testValues() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: valueOf(java.lang.String s) 
+* 
+*/ 
+@Test
+public void testValueOf() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
