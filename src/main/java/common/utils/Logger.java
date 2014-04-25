01b22347da71613e3cdfe10e6941ddb49d91afc8
Suparn <gupta.suparn@gmail.com>
2014-04-24 22:40:53 -0500

    Enabled Logging

--- src/main/java/common/utils/Logger.java
+++ src/main/java/common/utils/Logger.java
@@ -10,32 +10,32 @@ public class Logger {
     * Logs services at LOG Level
     * */
     public static void log(Object... messages) {
-//        System.out.println("*********************LOG Begins********************");
-//        for (Object o : messages) {
-//            System.out.println(o);
-//        }
-//        System.out.println("*********************LOG Ends********************");
+        System.out.println("*********************LOG Begins********************");
+        for (Object o : messages) {
+            System.out.println(o);
+        }
+        System.out.println("*********************LOG Ends********************");
     }
 
     /*
     * Logs services at DEBUG Level
     * */
     public static void debug(Object... messages) {
-//        System.out.println("*********************DEBUG LOG Begins********************");
-//        for (Object o : messages) {
-//            System.out.println(o);
-//        }
-//        System.out.println("*********************DEBUG LOG Ends********************");
+        System.out.println("*********************DEBUG LOG Begins********************");
+        for (Object o : messages) {
+            System.out.println(o);
+        }
+        System.out.println("*********************DEBUG LOG Ends********************");
     }
 
     /*
     * Logs services at ERROR Level
     * */
     public static void error(Object... messages) {
-//        System.err.println("*********************ERROR LOG Begins********************");
-//        for (Object o : messages) {
-//            System.err.println(o);
-//        }
-//        System.err.println("*********************ERROR LOG Ends********************");
+        System.err.println("*********************ERROR LOG Begins********************");
+        for (Object o : messages) {
+            System.err.println(o);
+        }
+        System.err.println("*********************ERROR LOG Ends********************");
     }
 }
